8f005530772225226774241dd8522832a7ece8c4
karadagserhat <serhatkaradagg@outlook.com>
2023-09-20 17:46:21 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import {
   HomeLayout,
@@ -14,6 +16,29 @@ import {
   Orders,
 } from './pages';
 
+import { ErrorElement } from './components';
+
+import { store } from './store';
+
+// loaders
+import { loader as landingLoader } from './pages/Landing';
+import { loader as singleProductLoader } from './pages/SingleProduct';
+import { loader as productsLoader } from './pages/Products';
+import { loader as checkoutLoader } from './pages/Checkout';
+import { loader as ordersLoader } from './pages/Orders';
+// actions
+import { action as registerAction } from './pages/Register';
+import { action as loginAction } from './pages/Login';
+import { action as checkoutAction } from './components/CheckoutForm';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
+
 const router = createBrowserRouter([
   {
     path: '/',
@@ -23,14 +48,20 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Landing />,
+        loader: landingLoader(queryClient),
+        errorElement: <ErrorElement />,
       },
       {
         path: 'products',
         element: <Products />,
+        loader: productsLoader(queryClient),
+        errorElement: <ErrorElement />,
       },
       {
         path: 'products/:id',
         element: <SingleProduct />,
+        loader: singleProductLoader(queryClient),
+        errorElement: <ErrorElement />,
       },
       {
         path: 'cart',
@@ -40,10 +71,13 @@ const router = createBrowserRouter([
       {
         path: 'checkout',
         element: <Checkout />,
+        loader: checkoutLoader(store),
+        action: checkoutAction(store, queryClient),
       },
       {
         path: 'orders',
         element: <Orders />,
+        loader: ordersLoader(store, queryClient),
       },
     ],
   },
@@ -51,16 +85,23 @@ const router = createBrowserRouter([
     path: '/login',
     element: <Login />,
     errorElement: <Error />,
+    action: loginAction(store),
   },
   {
     path: '/register',
     element: <Register />,
     errorElement: <Error />,
+    action: registerAction,
   },
 ]);
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 };
 
 export default App;
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,27 @@
+import FeaturedProducts from '../components/FeaturedProducts';
+import Hero from '../components/Hero';
+import { customFetch } from '../utils';
+
+const url = '/products?featured=true';
+
+const featuredProductsQuery = {
+  queryKey: ['featuredProducts'],
+  queryFn: () => customFetch(url),
+};
+
+export const loader = (queryClient) => async () => {
+  const response = await queryClient.ensureQueryData(featuredProductsQuery);
+  // console.log(response);
+  const products = response.data.data;
+  return { products };
+};
+
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <>
+      <Hero />
+      <FeaturedProducts />
+    </>
+  );
 };
 export default Landing;
